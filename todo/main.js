b25c5b3bbc8cbdf23610a2f6e9246b5dc6299612
bow16017 <97207297+bow16017@users.noreply.github.com>
2022-02-02 21:56:52 -0700

    Continued work on TODO application

--- todo/main.js
+++ todo/main.js
@@ -1,3 +1,65 @@
+var itemCount = 0;
+
+//make list item
+function newListItem() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("addTask").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("Please enter an item."); //replace with GUI later
+    } else {
+        document.getElementById("fList").appendChild(li);
+        this.itemCount = itemCount + 1;
+    }
+    document.getElementById("addTask").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < closeButton.length; i++) {
+        closeButton[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+            itemCount = itemCount -1;
+            document.getElementById("counter").innerHTML = itemCount;
+        }
+        document.getElementById("counter").innerHTML = itemCount;
+    }
+}
+
+//make new list item close
+var closeButton = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < closeButton.length; i++) {
+    closeButton[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+//checked off
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+
+
+
+
+
+/*
+
+this.itemCount = itemCount - 1;
+
+
+
 function addT() {
     
 }
@@ -12,4 +74,4 @@ function active() {
 
 function completed() {
     
-}
+}*/
---
+++ todo/utilities.js
@@ -0,0 +1,11 @@
+/*function MyObj() {
+    MyObj.count++;
+}
+
+MyObj.count = 0;
+
+var a = new MyObj;
+var b = new MyObj;
+
+
+document.getElementById("counter").innerHTML = MyObj.count;*/
