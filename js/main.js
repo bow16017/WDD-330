ab51513b2f61cc20332e4bef473139245ab17ccd
bow16017 <97207297+bow16017@users.noreply.github.com>
2022-02-09 12:49:25 -0700

    Finishing 'ToDo' and adding links

--- js/main.js
+++ js/main.js
@@ -18,6 +18,10 @@ const links = [
     {
 		label: 'Week5 notes',
 		url: 'week5/index.html'
+	},
+    {
+		label: 'ToDo Application',
+		url: 'todo/index.html'
 	}
 ];
 
--- todo/main.js
+++ todo/main.js
@@ -1,37 +1,36 @@
 var itemCount = 0;
-
-//make list item
+var tC = document.getElementById("completed");
+var tA = document.getElementById("active");
 function newListItem() {
-    var li = document.createElement("li");
+    var meaning = document.getElementById("addTask");
+    var li = document.createElement("object");
     var inputValue = document.getElementById("addTask").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
-        alert("Please enter an item."); //replace with GUI later
+        meaning.setAttribute("placeholder", "Please Enter A Task");
     } else {
+        li.setAttribute("name", "d");
         document.getElementById("fList").appendChild(li);
         this.itemCount = itemCount + 1;
     }
     document.getElementById("addTask").value = "";
-
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-
     for (i = 0; i < closeButton.length; i++) {
         closeButton[i].onclick = function () {
             var div = this.parentElement;
             div.style.display = "none";
-            itemCount = itemCount -1;
+            itemCount = itemCount - 1;
             document.getElementById("counter").innerHTML = itemCount;
         }
         document.getElementById("counter").innerHTML = itemCount;
     }
+    
 }
-
-//make new list item close
 var closeButton = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < closeButton.length; i++) {
@@ -40,38 +39,52 @@ for (i = 0; i < closeButton.length; i++) {
         div.style.display = "none";
     }
 }
-
-//checked off
-var list = document.querySelector('ul');
+list = document.querySelector('section');
 list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
+    if (ev.target.tagName === 'OBJECT') {
         ev.target.classList.toggle('checked');
     }
+    if (ev.target.classList.contains('checked')) {
+        ev.target.setAttribute("name", "c");
+    } else {
+        ev.target.setAttribute("name", "d");
+    }
 }, false);
-
-
-
-
-
-
-/*
-
-this.itemCount = itemCount - 1;
-
-
-
-function addT() {
-    
-}
-
-function all() {
-    
-}
-
-function active() {
-    
-}
-
 function completed() {
-    
-}*/
+    var nowArray = Array.prototype.slice.call(document.getElementsByName("c"));
+    if (tC.value == "True") {
+        tC.value = "False";
+        nowArray.map(function (i) {
+            i.classList.add('invisible');
+        });
+    } else if (tC.value == "False") {
+        tC.value = "True";
+        nowArray.map(function (i) {
+            i.classList.remove('invisible');
+        });
+    }
+}
+function active() {
+    var nowArray = Array.prototype.slice.call(document.getElementsByName("d"));
+    if (tA.value == "True") {
+        tA.value = "False";
+        nowArray.map(function (i) {
+            i.classList.add('invisible');
+        });
+    } else if (tA.value == "False") {
+        tA.value = "True";
+        nowArray.map(function (i) {
+            i.classList.remove('invisible');
+        });
+    }
+}
+function allr() {
+    var arrayrA = Array.prototype.slice.call(document.getElementsByName("c"));
+    var arrayrB = Array.prototype.slice.call(document.getElementsByName("d"));
+    arrayrA.map(function (i) {
+        i.classList.remove('invisible');
+    });
+    arrayrB.map(function (i) {
+        i.classList.remove('invisible');
+    });
+}
